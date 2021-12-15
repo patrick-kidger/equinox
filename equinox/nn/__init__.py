0f89d5a7cd11dbb06e48adab1f39391e6e3cd3bc
Andy Ehrenberg <32784181+andyehrenberg@users.noreply.github.com>
2021-12-15 13:50:48 -0800

    Update __init__.py

--- equinox/nn/__init__.py
+++ equinox/nn/__init__.py
@@ -1,5 +1,6 @@
 from .composed import MLP, Sequential
+from .conv import Conv1d, Conv2d, Conv3d, Conv
 from .dropout import Dropout
 from .linear import Identity, Linear
 from .rnn import GRUCell, LSTMCell
-from .conv import Conv1d, Conv2d, Conv3d, Conv
+
