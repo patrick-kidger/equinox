29ffde08fb51d0b94989634b4a93201b498c897d
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-02-09 21:28:37 +0000

    Updated pre-commit hooks

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,6 @@ import pytest
 def getkey():
     def _getkey():
         # Not sure what the maximum actually is but this will do
-        return jrandom.PRNGKey(random.randint(0, 2 ** 31 - 1))
+        return jrandom.PRNGKey(random.randint(0, 2**31 - 1))
 
     return _getkey
