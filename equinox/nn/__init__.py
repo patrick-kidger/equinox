7a7a9ed354e4d3251a45ef6ad33c80e54b25b280
Andy Ehrenberg <andyehrenberg@gmail.com>
2022-02-15 23:29:24 -0800

    conv transpose layers

--- equinox/nn/__init__.py
+++ equinox/nn/__init__.py
@@ -1,5 +1,14 @@
 from .composed import MLP, Sequential
-from .conv import Conv, Conv1d, Conv2d, Conv3d
+from .conv import (
+    Conv,
+    Conv1d,
+    Conv2d,
+    Conv3d,
+    ConvTranspose,
+    ConvTranspose1d,
+    ConvTranspose2d,
+    ConvTranspose3d,
+)
 from .dropout import Dropout
 from .linear import Identity, Linear
 from .rnn import GRUCell, LSTMCell
--- equinox/nn/conv.py
+++ equinox/nn/conv.py
@@ -6,7 +6,7 @@ import jax
 import jax.numpy as jnp
 import jax.random as jrandom
 import numpy as np
-from jax.lax import conv_general_dilated
+from jax.lax import conv_general_dilated, conv_transpose
 
 from ..custom_types import Array
 from ..module import Module, static_field
@@ -24,6 +24,52 @@ def _ntuple(n: int) -> callable:
         return tuple(repeat(x, n))
 
     return parse
+
+
+def compute_adjusted_padding(
+    input_size: int,
+    kernel_size: int,
+    stride: int,
+    padding: int,
+    output_padding: int,
+    dilation: int = 1,
+) -> Tuple[int, int]:
+    """Computes adjusted padding for desired ConvTranspose `output_padding`."""
+    output_size = (
+        (input_size - 1) * stride
+        - 2 * padding
+        + dilation * (kernel_size - 1)
+        + output_padding
+        + 1
+    )
+    kernel_size = (kernel_size - 1) * dilation + 1
+    if padding == 0:
+        expected_input_size = (output_size - kernel_size + stride) // stride
+        if input_size != expected_input_size:
+            raise ValueError(
+                f"The expected input size with the current set of input "
+                f"parameters is {expected_input_size} which doesn't "
+                f"match the actual input size {input_size}."
+            )
+        padding_before = 0
+    elif padding == 1:
+        expected_input_size = (output_size + stride - 1) // stride
+        if input_size != expected_input_size:
+            raise ValueError(
+                f"The expected input size with the current set of input "
+                f"parameters is {expected_input_size} which doesn't "
+                f"match the actual input size {input_size}."
+            )
+        padding_needed = max(0, (input_size - 1) * stride + kernel_size - output_size)
+        padding_before = padding_needed // 2
+    else:
+        raise ValueError(f"`padding` must be '0' or '1'. Passed: {padding}.")
+
+    expanded_input_size = (input_size - 1) * stride + 1
+    padded_out_size = output_size + kernel_size - 1
+    pad_before = kernel_size - 1 - padding_before
+    pad_after = padded_out_size - expanded_input_size - pad_before
+    return (pad_before, pad_after)
 
 
 class Conv(Module):
@@ -240,6 +286,254 @@ class Conv3d(Conv):
             out_channels=out_channels,
             kernel_size=kernel_size,
             stride=stride,
+            padding=padding,
+            dilation=dilation,
+            use_bias=use_bias,
+            key=key,
+            **kwargs,
+        )
+
+
+class ConvTranspose(Module):
+    """General N dimension Transpose Convolution"""
+
+    num_spatial_dims: int = static_field()
+    weight: Array
+    bias: Optional[Array]
+    in_channels: int = static_field()
+    out_channels: int = static_field()
+    kernel_size: Tuple[int] = static_field()
+    stride: Tuple[int] = static_field()
+    padding: Tuple[int] = static_field()
+    output_padding: Tuple[int] = static_field()
+    dilation: Tuple[int] = static_field()
+    use_bias: bool = static_field()
+    dimension_numbers: Tuple[str] = static_field()
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        padding: Union[int, Sequence[int]] = 0,
+        output_padding: Union[int, Sequence[int]] = 0,
+        dilation: Union[int, Sequence[int]] = 1,
+        use_bias: bool = True,
+        *,
+        key: "jax.random.PRNGKey",
+        **kwargs,
+    ):
+        """**Arguments:**
+
+        - `num_spatial_dims`: The number of spatial dimensions. For example traditional
+            convolutions for image processing have this set to `2`.
+        - `in_channels`: The number of input channels.
+        - `out_channels`: The number of output channels.
+        - `kernel_size`: The size of the transposed convolutional kernel.
+        - `stride`: The stride of the transposed convolution.
+        - `padding`: The amount of implicit padding on both sides for `dilation *
+            (kernel_size - 1) - padding points.
+        - `output_padding`: The additional size added to the output shape.
+        - `dilation`: The spacing between kernel points.
+        - `use_bias`: Whether to add on a bias after the transposed convolution.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+
+        !!! info
+
+            All of `kernel_size`, `stride`, `padding`, `output_padding`, `dilation` can
+            be either an integer or a sequence of integers. If they are a sequence then
+            the sequence should be of length equal to `num_spatial_dims`, and specify
+            the value of each property down each spatial dimension in turn.. If they
+            are an integer then the same kernel size / stride / padding / dilation will
+            be used along every spatial dimension.
+
+        """
+        super().__init__(**kwargs)
+        self.num_spatial_dims = num_spatial_dims
+        parse = _ntuple(self.num_spatial_dims)
+        wkey, bkey = jrandom.split(key, 2)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = parse(kernel_size)
+        self.use_bias = use_bias
+        self.output_padding = parse(output_padding)
+        self.padding = parse(padding)
+        lim = 1 / np.sqrt(self.in_channels * np.prod(self.kernel_size))
+        if self.num_spatial_dims == 1:
+            self.dimension_numbers = ("NCH", "IOH", "NCH")
+        elif self.num_spatial_dims == 2:
+            self.dimension_numbers = ("NCHW", "IOHW", "NCHW")
+        elif self.num_spatial_dims == 3:
+            self.dimension_numbers = ("NCDHW", "IODHW", "NCDHW")
+        else:
+            raise ValueError(
+                "`ConvTranspose` only supports between 1 and 3 spatial dims",
+                f"({self.num_spatial_dims} was given)",
+            )
+        self.weight = jrandom.uniform(
+            wkey,
+            (
+                self.in_channels,
+                self.out_channels,
+            )
+            + self.kernel_size,
+            minval=-lim,
+            maxval=lim,
+        )
+        if self.use_bias:
+            self.bias = jrandom.uniform(
+                bkey,
+                (self.out_channels,) + (1,) * self.num_spatial_dims,
+                minval=-lim,
+                maxval=lim,
+            )
+        else:
+            self.bias = None
+
+        self.stride = parse(stride)
+        self.dilation = parse(dilation)
+
+    def __call__(
+        self, x: Array, *, key: Optional["jax.random.PRNGKey"] = None
+    ) -> Array:
+        """**Arguments:**
+
+        - `x`: The input. Should be a JAX array of shape `(in_channels, dim_1, ..., dim_N)`, where
+            `N = num_spatial_dims`.
+        - `key`: Ignored; provided for compatibility with the rest of the Equinox API.
+            (Keyword only argument.)
+
+        **Returns:**
+
+        A JAX array of shape `(out_channels, new_dim_1, ..., new_dim_N)`.
+        """
+        unbatched_rank = self.num_spatial_dims + 1
+        if x.ndim != unbatched_rank:
+            raise ValueError(
+                f"Input to `ConvTranspose` needs to have rank {unbatched_rank},",
+                f" but input has shape {x.shape}.",
+            )
+        x = jnp.expand_dims(x, axis=0)
+        padding = self.padding
+        if self.output_padding is not None:
+            padding = tuple(
+                map(
+                    lambda s, k, st, p, o, d: compute_adjusted_padding(
+                        s, k, st, p, o, d
+                    ),
+                    x.shape[2:],
+                    self.weight.shape[2:],
+                    self.stride,
+                    self.padding,
+                    self.output_padding,
+                    self.dilation,
+                )
+            )
+        x = conv_transpose(
+            lhs=x,
+            rhs=self.weight,
+            strides=self.stride,
+            padding=padding,
+            rhs_dilation=self.dilation,
+            dimension_numbers=self.dimension_numbers,
+        )
+        if self.use_bias:
+            x += jnp.broadcast_to(self.bias, x.shape)
+        x = jnp.squeeze(x, axis=0)
+        return x
+
+
+class ConvTranspose1d(ConvTranspose):
+    """As [`equinox.nn.ConvTranspose`][] with `num_spatial_dims=1`."""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        output_padding=None,
+        padding=0,
+        dilation=1,
+        use_bias=True,
+        *,
+        key,
+        **kwargs,
+    ):
+        super().__init__(
+            num_spatial_dims=1,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            output_padding=output_padding,
+            padding=padding,
+            dilation=dilation,
+            use_bias=use_bias,
+            key=key,
+            **kwargs,
+        )
+
+
+class ConvTranspose2d(ConvTranspose):
+    """As [`equinox.nn.ConvTranspose`][] with `num_spatial_dims=2`."""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=(1, 1),
+        output_padding=None,
+        padding=(0, 0),
+        dilation=(1, 1),
+        use_bias=True,
+        *,
+        key,
+        **kwargs,
+    ):
+        super().__init__(
+            num_spatial_dims=2,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            output_padding=output_padding,
+            padding=padding,
+            dilation=dilation,
+            use_bias=use_bias,
+            key=key,
+            **kwargs,
+        )
+
+
+class ConvTranspose3d(ConvTranspose):
+    """As [`equinox.nn.ConvTranspose`][] with `num_spatial_dims=3`."""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=(1, 1, 1),
+        output_padding=None,
+        padding=(0, 0, 0),
+        dilation=(1, 1, 1),
+        use_bias=True,
+        *,
+        key,
+        **kwargs,
+    ):
+        super().__init__(
+            num_spatial_dims=3,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            output_padding=output_padding,
             padding=padding,
             dilation=dilation,
             use_bias=use_bias,
