d192b3fab0abecfea03edd33dd18bbefb0d276ae
Andy Ehrenberg <andyehrenberg@gmail.com>
2022-02-16 00:03:08 -0800

    conv transpose tests

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -254,3 +254,212 @@ def test_conv3d(getkey):
     conv = eqx.tree_at(lambda x: (x.weight, x.bias), conv, (new_weight, new_bias))
     answer = jnp.array([-3, -2, -1, 0, 1, 2, 3, 4, 1, 1, 1, 1]).reshape(1, 3, 2, 2)
     assert jnp.allclose(conv(data), answer)
+
+
+def test_convtranspose1d(getkey):
+    # Positional arguments
+    conv = eqx.nn.ConvTranspose1d(1, 3, 3, key=getkey())
+    x = jrandom.normal(getkey(), (1, 32))
+    assert conv(x).shape == (3, 34)
+
+    # Some keyword arguments
+    conv = eqx.nn.ConvTranspose1d(1, out_channels=3, kernel_size=(3,), key=getkey())
+    x = jrandom.normal(getkey(), (1, 32))
+    assert conv(x).shape == (3, 34)
+
+    # All keyword arguments
+    conv = eqx.nn.ConvTranspose1d(
+        in_channels=1,
+        out_channels=3,
+        kernel_size=(3,),
+        padding=0,
+        output_padding=0,
+        use_bias=False,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (1, 32))
+    assert conv(x).shape == (3, 34)
+
+    # Test strides
+    conv = eqx.nn.ConvTranspose1d(
+        in_channels=3,
+        out_channels=1,
+        kernel_size=(3,),
+        stride=2,
+        padding=1,
+        output_padding=1,
+        use_bias=True,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (3, 32))
+    assert conv(x).shape == (1, 64)
+
+    # Test value matches
+    conv = eqx.nn.ConvTranspose1d(1, 3, kernel_size=3, padding=0, key=getkey())
+    new_weight = jnp.arange(9).reshape(1, 3, 3)
+    new_bias = jnp.array([1, 2, 3]).reshape(3, 1)
+    data = jnp.arange(-3, 3).reshape(1, -1)
+    assert new_weight.shape == conv.weight.shape
+    assert new_bias.shape == conv.bias.shape
+    conv = eqx.tree_at(lambda x: (x.weight, x.bias), conv, (new_weight, new_bias))
+    answer = jnp.array(
+        [
+            -5,
+            -6,
+            -3,
+            0,
+            3,
+            6,
+            3,
+            1,
+            -13,
+            -20,
+            -20,
+            -8,
+            4,
+            16,
+            13,
+            8,
+            -21,
+            -34,
+            -37,
+            -16,
+            5,
+            26,
+            23,
+            15,
+        ]
+    ).reshape(3, 8)
+    assert jnp.allclose(conv(data), answer)
+
+
+def test_convtranspose2d(getkey):
+    # Positional arguments
+    conv = eqx.nn.ConvTranspose2d(1, 3, 3, key=getkey())
+    x = jrandom.normal(getkey(), (1, 32, 32))
+    assert conv(x).shape == (3, 34, 34)
+
+    # Some keyword arguments
+    conv = eqx.nn.ConvTranspose2d(1, out_channels=3, kernel_size=(3, 3), key=getkey())
+    x = jrandom.normal(getkey(), (1, 32, 32))
+    assert conv(x).shape == (3, 34, 34)
+
+    # All keyword arguments
+    conv = eqx.nn.ConvTranspose2d(
+        in_channels=1,
+        out_channels=3,
+        kernel_size=(3, 3),
+        padding=1,
+        use_bias=False,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (1, 32, 32))
+    assert conv(x).shape == (3, 32, 32)
+
+    # Test strides
+    conv = eqx.nn.ConvTranspose2d(
+        in_channels=3,
+        out_channels=1,
+        kernel_size=(3, 3),
+        stride=2,
+        padding=1,
+        output_padding=1,
+        use_bias=True,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (3, 32, 32))
+    assert conv(x).shape == (1, 64, 64)
+
+    # Test value matches
+    conv = eqx.nn.ConvTranspose2d(1, 1, kernel_size=3, padding=1, key=getkey())
+    new_weight = jnp.arange(9).reshape(1, 1, 3, 3)
+    new_bias = jnp.array([1]).reshape(1, 1, 1)
+    data = jnp.arange(-4, 5).reshape(1, 3, 3)
+    assert new_weight.shape == conv.weight.shape
+    assert new_bias.shape == conv.bias.shape
+    conv = eqx.tree_at(lambda x: (x.weight, x.bias), conv, (new_weight, new_bias))
+    answer = jnp.array([-37, -31, -9, 25, 61, 49, 23, 41, 27]).reshape(1, 3, 3)
+    assert jnp.allclose(conv(data), answer)
+
+
+def test_convtranspose3d(getkey):
+    # Positional arguments
+    conv = eqx.nn.ConvTranspose3d(1, 3, 3, key=getkey())
+    x = jrandom.normal(getkey(), (1, 3, 32, 32))
+    assert conv(x).shape == (3, 5, 34, 34)
+
+    # Some keyword arguments
+    conv = eqx.nn.ConvTranspose3d(
+        1, out_channels=3, kernel_size=(3, 3, 3), key=getkey()
+    )
+    x = jrandom.normal(getkey(), (1, 3, 32, 32))
+    assert conv(x).shape == (3, 5, 34, 34)
+
+    # All keyword arguments
+    conv = eqx.nn.ConvTranspose3d(
+        in_channels=1,
+        out_channels=3,
+        kernel_size=(3, 3, 3),
+        padding=1,
+        use_bias=False,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (1, 3, 32, 32))
+    assert conv(x).shape == (3, 3, 32, 32)
+
+    # Test strides
+    conv = eqx.nn.ConvTranspose3d(
+        in_channels=3,
+        out_channels=1,
+        kernel_size=(3, 3, 3),
+        stride=2,
+        padding=1,
+        output_padding=1,
+        use_bias=True,
+        key=getkey(),
+    )
+    x = jrandom.normal(getkey(), (3, 3, 32, 32))
+    assert conv(x).shape == (1, 6, 64, 64)
+
+    # Test value matches
+    conv = eqx.nn.ConvTranspose3d(
+        1, 1, kernel_size=(2, 2, 2), padding=(0, 0, 0), key=getkey()
+    )
+    new_weight = jnp.arange(8).reshape(1, 1, 2, 2, 2)
+    new_bias = jnp.array([1]).reshape(1, 1, 1, 1)
+    data = jnp.arange(-4, 4).reshape(1, 2, 2, 2)
+    assert new_weight.shape == conv.weight.shape
+    assert new_bias.shape == conv.bias.shape
+    conv = eqx.tree_at(lambda x: (x.weight, x.bias), conv, (new_weight, new_bias))
+    answer = jnp.array(
+        [
+            -27,
+            -44,
+            -17,
+            -33,
+            -49,
+            -17,
+            -9,
+            -12,
+            -3,
+            -11,
+            -9,
+            1,
+            5,
+            29,
+            21,
+            9,
+            23,
+            13,
+            1,
+            4,
+            3,
+            7,
+            15,
+            7,
+            3,
+            4,
+            1,
+        ]
+    ).reshape(1, 3, 3, 3)
+    assert jnp.allclose(conv(data), answer)
