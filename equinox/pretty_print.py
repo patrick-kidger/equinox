ca50787a691edee2c518f01eeb5e530e1e2a5dfa
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-04-06 17:08:28 +0100

    Added tree_pformat(truncate_leaf=...)

--- equinox/pretty_print.py
+++ equinox/pretty_print.py
@@ -1,7 +1,7 @@
 import dataclasses
 import functools as ft
 import types
-from typing import Any, Dict, List, NamedTuple, Tuple
+from typing import Any, Callable, Dict, List, NamedTuple, Tuple
 
 import jax
 import jax._src.pretty_printer as pp
@@ -138,7 +138,10 @@ def _pformat_function(obj: types.FunctionType, **kwargs) -> pp.Doc:
 # Ideally this would be something tied to being a PyTree.
 def _pformat(obj: PrettyPrintAble, **kwargs) -> pp.Doc:
     follow_wrapped = kwargs["follow_wrapped"]
-    if dataclasses.is_dataclass(obj):
+    truncate_leaf = kwargs["truncate_leaf"]
+    if truncate_leaf(obj):
+        return pp.text(f"{type(obj).__name__}(...)")
+    elif dataclasses.is_dataclass(obj):
         return _pformat_dataclass(obj, **kwargs)
     elif isinstance(obj, list):
         return _pformat_list(obj, **kwargs)
@@ -165,12 +168,17 @@ def _pformat(obj: PrettyPrintAble, **kwargs) -> pp.Doc:
         return pp.text(repr(obj))
 
 
+def _false(_):
+    return False
+
+
 def tree_pformat(
     pytree: PrettyPrintAble,
     width: int = 80,
     indent: int = 2,
     short_arrays: bool = True,
     follow_wrapped: bool = True,
+    truncate_leaf: Callable[[PrettyPrintAble], bool] = _false,
 ) -> str:
     """Pretty-formats a PyTree as a string, whilst abbreviating JAX arrays.
 
@@ -192,6 +200,8 @@ def tree_pformat(
     - `indent`: The amount of indentation each nesting level.
     - `short_arrays`: Toggles the abbreviation of JAX arrays.
     - `follow_wrapped`: Whether to unwrap `functools.partial` and `functools.wraps`.
+    - `truncate_leaf`: A function `Any -> bool`. Applied to all nodes in the PyTree;
+        all truthy nodes will be truncated to just `f"{type(node).__name__}(...)"`.
 
     **Returns:**
 
@@ -199,5 +209,9 @@ def tree_pformat(
     """
 
     return _pformat(
-        pytree, indent=indent, short_arrays=short_arrays, follow_wrapped=follow_wrapped
+        pytree,
+        indent=indent,
+        short_arrays=short_arrays,
+        follow_wrapped=follow_wrapped,
+        truncate_leaf=truncate_leaf,
     ).format(width=width)
