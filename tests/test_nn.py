2ba23e46cc3f439869686e069b1dfad45661a72c
uuirs <3000cl@gmail.com>
2022-09-06 00:11:22 +0000

    formatted by black

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -579,7 +579,10 @@ def test_multihead_attention(getkey):
 
     x = jnp.arange(1, 13).reshape(3, 4)
     mask = jnp.broadcast_to(jnp.array([True, False, False]), (2, 3, 3))
-    assert jnp.allclose(attn(x, x, x, mask), jnp.broadcast_to(jnp.array([[680.0, 1960.0, 3240.0, 4520.0]]), (3, 4))) 
+    assert jnp.allclose(
+        attn(x, x, x, mask),
+        jnp.broadcast_to(jnp.array([[680.0, 1960.0, 3240.0, 4520.0]]), (3, 4)),
+    )
 
 
 def test_embedding(getkey):
