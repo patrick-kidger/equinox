039f6bf69125196dd2357ecf7e082d2f1bcc422c
uuirs <3000cl@gmail.com>
2022-11-19 02:37:46 +0800

    fix tree_inference (#233)

--- equinox/tree.py
+++ equinox/tree.py
@@ -253,21 +253,17 @@ def tree_equal(*pytrees: PyTree) -> Union[bool, np.bool_, Bool[Array, ""]]:
     return out
 
 
-def _subinferences(pytree):
-    is_leaf = lambda x: hasattr(x, "inference") and x is not pytree
-    out = [x for x in jtu.tree_leaves(pytree, is_leaf=is_leaf) if is_leaf(x)]
-    for x in out:
-        out.extend(_subinferences(x))
-    return out
-
-
 def _inferences(pytree):
-    is_leaf = lambda x: hasattr(x, "inference")
-    out = [x for x in jtu.tree_leaves(pytree, is_leaf=is_leaf) if is_leaf(x)]
+    is_leaf = lambda x: hasattr(x, "inference") and x is not pytree
+
+    out = [pytree.inference] if hasattr(pytree, "inference") else []
+
+    leaves = [x for x in jtu.tree_leaves(pytree, is_leaf=is_leaf) if is_leaf(x)]
     # Nodes with an inference flag might have sub-nodes with an inference flag.
-    for x in out:
-        out.extend(_subinferences(x))
-    return [x.inference for x in out]
+
+    for x in leaves:
+        out.extend(_inferences(x))
+    return out
 
 
 def tree_inference(pytree: PyTree, value: bool) -> PyTree:
