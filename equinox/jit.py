a6bd2e9f9f418c8a80636ec8a2459ad0dcbdf2bd
uuirs <3000cl@gmail.com>
2022-12-02 09:55:46 +0000

    simplify filter_jit; example fixes;

--- equinox/jit.py
+++ equinox/jit.py
@@ -14,33 +14,21 @@ from .compile_utils import (
     hashable_combine,
     hashable_partition,
 )
-from .custom_types import BoolAxisSpec, sentinel, TreeDef
+from .custom_types import sentinel, TreeDef
 from .doc_utils import doc_strip_annotations
 from .filters import combine, is_array, partition
 from .module import Module, module_update_wrapper, Static
 
 
-def _hashable_partition(pytree, donate_default):
-    donate_or_dynamic_part, static_part = hashable_partition(pytree, is_array)
-    donate_part, dynamic_part = partition(donate_or_dynamic_part, donate_default)
-    return donate_part, dynamic_part, static_part
-
-
-def _hashable_combine(donate_part, dynamic_part, static_part):
-    donate_or_dynamic_part = combine(donate_part, dynamic_part)
-    return hashable_combine(donate_or_dynamic_part, static_part)
-
-
 @compile_cache
-def _filter_jit_cache(fun_names, **jitkwargs):
-    def fun_wrapped(dynamic, static, donate):
+def _filter_jit_cache(fun_names, donate_default, **jitkwargs):
+    def fun_wrapped(dynamic, static):
         dynamic_fun, dynamic_spec = dynamic
         static_fun, static_spec = static
-        donate_fun, donate_spec = donate
 
-        fun = _hashable_combine(donate_fun, dynamic_fun, static_fun)
+        fun = hashable_combine(dynamic_fun, static_fun)
 
-        args, kwargs = _hashable_combine(donate_spec, dynamic_spec, static_spec)
+        args, kwargs = hashable_combine(dynamic_spec, static_spec)
         out = fun(*args, **kwargs)
         dynamic_out, static_out = partition(out, is_array)
         return dynamic_out, Static(static_out)
@@ -49,15 +37,16 @@ def _filter_jit_cache(fun_names, **jitkwargs):
     fun_wrapped.__name__ = fun_name
     fun_wrapped.__qualname__ = fun_qualname
 
-    return jax.jit(fun_wrapped, static_argnums=1, donate_argnums=2, **jitkwargs)
+    if donate_default:
+        return jax.jit(fun_wrapped, static_argnums=1, donate_argnums=0, **jitkwargs)
+    else:
+        return jax.jit(fun_wrapped, static_argnums=1, **jitkwargs)
 
 
 class _JitWrapper(Module):
     _signature: inspect.Signature
     _dynamic_fun: PyTree[Any]
     _static_fun: Tuple[TreeDef, Sequence[Any]]
-    _donate_fun: PyTree[Any]
-    _donate_default: BoolAxisSpec
     _cached: FunctionType
     _filter_warning: bool
 
@@ -67,16 +56,13 @@ class _JitWrapper(Module):
         args = bound.args
         kwargs = bound.kwargs
 
-        donate_spec, dynamic_spec, static_spec = _hashable_partition(
-            (args, kwargs), self._donate_default
-        )
+        dynamic_spec, static_spec = hashable_partition((args, kwargs), is_array)
         dynamic = (self._dynamic_fun, dynamic_spec)
         static = (self._static_fun, static_spec)
-        donate = (self._donate_fun, donate_spec)
         if is_lower:
-            return self._cached.lower(dynamic, static, donate)
+            return self._cached.lower(dynamic, static)
         else:
-            dynamic_out, static_out = self._cached(dynamic, static, donate)
+            dynamic_out, static_out = self._cached(dynamic, static)
             return combine(dynamic_out, static_out.value)
 
     def __call__(__self, *args, **kwargs):
@@ -157,15 +143,13 @@ def filter_jit(
     donate_default = donate_default_dict[donate]
     filter_warning = True if donate == "arrays" else False
 
-    donate_fun, dynamic_fun, static_fun = _hashable_partition(fun, donate_default)
-    cached = _filter_jit_cache(get_fun_names(fun), **jitkwargs)
+    dynamic_fun, static_fun = hashable_partition(fun, is_array)
+    cached = _filter_jit_cache(get_fun_names(fun), donate_default, **jitkwargs)
 
     jit_wrapper = _JitWrapper(
         _signature=signature,
         _dynamic_fun=dynamic_fun,
         _static_fun=static_fun,
-        _donate_fun=donate_fun,
-        _donate_default=donate_default,
         _cached=cached,
         _filter_warning=filter_warning,
     )
