d431d56cb69699fa949377baf218e8d4163b8ea9
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-05-07 10:07:16 -0700

    LayerNorm and GroupNorm now have API compatibility with BatchNorm

--- equinox/_custom_types.py
+++ equinox/_custom_types.py
@@ -1,4 +1,5 @@
 from typing import Any, TYPE_CHECKING
+from typing_extensions import TypeAlias
 
 import jax.random as jr
 from jaxtyping import Array
@@ -8,6 +9,6 @@ from ._doc_utils import doc_repr
 
 sentinel: Any = doc_repr(object(), "sentinel")
 if TYPE_CHECKING:
-    PRNGKey = jr.KeyArray
+    PRNGKey: TypeAlias = jr.KeyArray
 else:
     PRNGKey = doc_repr(Array, "jax.random.PRNGKey")
