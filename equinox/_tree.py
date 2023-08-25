086aa88b884496e77b719c18bd4c19959bb9eeb3
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-08-25 18:57:40 +0100

    Fix broken link

--- equinox/_tree.py
+++ equinox/_tree.py
@@ -106,7 +106,7 @@ def tree_at(
         get_last_layer = lambda m: m.layers[-1]
         new_mlp = eqx.tree_at(get_last_layer, mlp, new_linear)
         ```
-        See also the [Tricks](../../../tricks) page.
+        See also the [Tricks](../../tricks) page.
     """  # noqa: E501
 
     # We need to specify a particular node in a PyTree.
