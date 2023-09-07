3c97e17d005d8a2a05a3541ad8d31109d1e34608
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-09-07 08:12:27 -0700

    EQX_ON_ERROR=breakpoint now produces a warning, to help avoid folks leaving it on permanently by accident.

--- equinox/_config.py
+++ equinox/_config.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 from typing import Literal
 
 
@@ -9,4 +10,9 @@ if EQX_ON_ERROR not in ("raise", "breakpoint", "nan"):
     raise ValueError(
         "Unrecognised value for `EQX_ON_ERROR`. Valid values are `EQX_ON_ERROR=raise`, "
         "`EQX_ON_ERROR=breakpoint`, and `EQX_ON_ERROR=nan`."
+    )
+if EQX_ON_ERROR == "breakpoint":
+    warnings.warn(
+        "The environment variable `EQX_ON_ERROR=breakpoint` is currently set. Note "
+        "that this should only be used for debugging, as it slows down runtime speed."
     )
