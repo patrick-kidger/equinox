5f159d03340c79dd1a92cedbc121ff8727819425
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-09-10 21:31:23 -0700

    filter_eval_shape now alwasys has a .out_struct property
    Previously, this was erroneously skipped when the function lacked any closed-over variables.
    In addition, this commit adds eqxi.cached_filter_eval_shape, as that is needed for the above.

--- equinox/_eval_shape.py
+++ equinox/_eval_shape.py
@@ -4,8 +4,10 @@ from typing import Any, Union
 
 import jax
 import jax._src.traceback_util as traceback_util
+import jax.tree_util as jtu
 from jaxtyping import PyTree
 
+from ._caches import internal_lru_caches
 from ._filters import combine, is_array, partition
 from ._module import Static
 
@@ -35,3 +37,26 @@ def filter_eval_shape(
     dynamic, static = partition((fun, args, kwargs), _filter)
     dynamic_out, static_out = jax.eval_shape(ft.partial(_fn, static), dynamic)
     return combine(dynamic_out, static_out.value)
+
+
+def _to_struct(x):
+    if is_array(x):
+        return jax.ShapeDtypeStruct(x.shape, x.dtype)
+    else:
+        return x
+
+
+@ft.lru_cache(maxsize=None)
+def _cached_filter_eval_shape(leaves, treedef):
+    fn, args, kwargs = jtu.tree_unflatten(treedef, leaves)
+    return filter_eval_shape(fn, *args, **kwargs)
+
+
+internal_lru_caches.append(_cached_filter_eval_shape)
+
+
+def cached_filter_eval_shape(fn, *args, **kwargs):
+    tree = jtu.tree_map(_to_struct, (fn, args, kwargs))
+    leaves, treedef = jtu.tree_flatten(tree)
+    leaves = tuple(leaves)
+    return _cached_filter_eval_shape(leaves, treedef)
