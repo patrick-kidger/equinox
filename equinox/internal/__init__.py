39d3a2c4082ecd915aa5bc7477e07500fbfe706c
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-10-18 17:29:46 -0700

    Added a MaybeBuffer annotation.
    This is for use as a dataclass annotation, as jaxtyping
    will verify that the type is correct.
    
    I'm not super happy with this. Buffers are getting increasingly
    complicated! I suspect a better resolution will be to move Buffers into
    Quax and treat them as array-ish objects, but that's fairly
    complicated.

--- equinox/internal/__init__.py
+++ equinox/internal/__init__.py
@@ -55,6 +55,7 @@ from ._finalise_jaxpr import (
 from ._loop import (
     buffer_at_set as buffer_at_set,
     maybe_set_p as maybe_set_p,
+    MaybeBuffer as MaybeBuffer,
     scan as scan,
     select_if_vmap_p as select_if_vmap_p,
     while_loop as while_loop,
