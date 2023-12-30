598c8aa844ff64f32f49a3443f370096756e9dd8
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-12-29 18:56:56 -0800

    Tweaks to WeightNorm

--- equinox/nn/_weight_norm.py
+++ equinox/nn/_weight_norm.py
@@ -1,10 +1,10 @@
+import functools as ft
 from collections.abc import Callable
-from functools import partial
 from typing import Generic, Optional, TypeVar
 
 import jax
 import jax.numpy as jnp
-from jaxtyping import Array, PRNGKeyArray, PyTree
+from jaxtyping import Array, PRNGKeyArray, Scalar
 
 from .._module import field, Module
 from .._tree import tree_at
@@ -13,9 +13,7 @@ from .._tree import tree_at
 _Layer = TypeVar("_Layer")
 
 
-def _norm_except_axis(
-    v: Array, norm: Callable[[PyTree[Array]], Array], axis: Optional[int]
-):
+def _norm_except_axis(v: Array, norm: Callable[[Array], Scalar], axis: Optional[int]):
     if axis is None:
         return norm(v)
     else:
@@ -23,17 +21,25 @@ def _norm_except_axis(
 
 
 class WeightNorm(Module, Generic[_Layer]):
-    r"""
-    Applies weight normalisation to a given parameter.
+    r"""Applies weight normalisation to a given parameter.
 
-    Given a weight matrix $\mathbf{W}$, computes the follow reparametrization:
+    Given the 2D weight matrix
+    $W = (W_{ij})_{ij} \in \mathbb{R}^{\text{out}} \times \mathbb{R}^{\text{in}}$ of a
+    linear layer, then it replaces it with the following reparameterisation:
 
-    $\mathbf{W} = g \frac{\mathbf{v}}{\lVert \mathbf{v} \rVert}$
+    $g \frac{v_{ij}}{\lVert v_{i\, \cdot} \rVert} \in \mathbb{R}^{\text{out}} \times \mathbb{R}^{\text{in}}$
 
-    where $g$ is initially chosen to equal $\lVert \mathbf{v} \rVert$
-    , and $\mathbf{v}$ is initially chosen as $\mathbf{W}$ .
+    where $v_{ij}$ is initialised as $W_{ij}$, and $g$ is initialised as
+    $\lVert v_{i\, \cdot} \rVert = \sum_j {v_{ij}}^2$.
+
+    Overall, the direction ($v$) and the magnitude ($g$) of the output of each neuron
+    are treated separately.
+
+    Given n-dimensional weight matrices $W$ (in convolutional layers), then the
+    normalisation is analogusly instead computed over every axis except the first.
 
     ??? cite
+
         [Weight Normalisation](https://arxiv.org/abs/1602.07868)
 
         ```bibtex
@@ -54,13 +60,13 @@ class WeightNorm(Module, Generic[_Layer]):
         bibsource    = {dblp computer science bibliography, https://dblp.org}
         }
         ```
-    """
+    """  # noqa: E501
 
     layer: _Layer
     g: Array
     weight_name: str = field(static=True)
     axis: Optional[int] = field(static=True)
-    _norm: Callable[[Array], Array]
+    _norm: Callable[[Array], Scalar]
 
     def __init__(
         self,
@@ -71,18 +77,20 @@ class WeightNorm(Module, Generic[_Layer]):
         """**Arguments:**
 
         - `layer`: The layer to wrap. Usually a [`equinox.nn.Linear`][] or
-        a convolutional layer (e.g. [`equinox.nn.Conv2d`][]).
+            a convolutional layer (e.g. [`equinox.nn.Conv2d`][]).
         - `weight_name`: The name of the layer's parameter (a JAX array) to apply
-        weight normalisation to.
+            weight normalisation to.
         - `axis`: The norm is computed across every axis except this one.
-        If `None`, compute across every axis.
+            If `None`, compute across every axis.
         """
         self.layer = layer
         self.weight_name = weight_name
         self.axis = axis
 
-        self._norm = partial(
-            _norm_except_axis, norm=partial(jnp.linalg.norm, keepdims=True), axis=axis
+        self._norm = ft.partial(
+            _norm_except_axis,
+            norm=ft.partial(jnp.linalg.norm, keepdims=True),
+            axis=axis,
         )
         self.g = self._norm(getattr(layer, weight_name))
 
