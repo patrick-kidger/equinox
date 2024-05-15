62304f5c5d1aaf0e45fcc9c27ba6a54f0e358b4e
Jake VanderPlas <jakevdp@google.com>
2024-05-15 08:53:18 -0700

    Avoid use of deprecated jax.tree_map

--- tests/test_sharding.py
+++ tests/test_sharding.py
@@ -1,6 +1,7 @@
 import equinox as eqx
 import jax
 import jax.random as jr
+import jax.tree_util as jtu
 
 
 [cpu] = jax.local_devices(backend="cpu")
@@ -15,7 +16,7 @@ def test_sharding():
     @eqx.filter_jit
     def f(x):
         a, b = eqx.partition(x, eqx.is_array)
-        a = jax.tree_map(lambda x: x + 1, a)
+        a = jtu.tree_map(lambda x: x + 1, a)
         x = eqx.combine(a, b)
         return eqx.filter_shard(x, sharding)
 
