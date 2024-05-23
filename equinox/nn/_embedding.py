1f7908bf92466bd1beea326e2935e59b38819e72
Artur A. Galstyan <63471891+Artur-Galstyan@users.noreply.github.com>
2024-05-23 13:52:39 +0200

    Update _embedding.py RopeEmbeddings (#735)
    * Update _embedding.py RopeEmbeddings
    
    It seems we forgot to make the `theta` variable public. This way, if you wanted to implement LLaMa3, you'd have a problem because they use a `theta` value of 50000.0.
    
    * pch
    
    * default args
    
    * theta was actually used; fixed now

--- equinox/nn/_embedding.py
+++ equinox/nn/_embedding.py
@@ -161,6 +161,7 @@ class RotaryPositionalEmbedding(Module, strict=True):
     """
 
     embedding_size: int = field(static=True)
+    theta: float = field(static=True, default=10_000.0)
 
     def __check_init__(self):
         if self.embedding_size < 0:
@@ -175,7 +176,7 @@ class RotaryPositionalEmbedding(Module, strict=True):
 
     @staticmethod
     def precompute_freqs_cis(
-        embedding_size: int, end: int, theta: float = 10000.0
+        embedding_size: int, end: int, theta: float
     ) -> Complex[Array, "end half_of_embedding_size"]:
         freqs = 1.0 / (
             theta
@@ -220,12 +221,16 @@ class RotaryPositionalEmbedding(Module, strict=True):
                 freqs_cis = internal_rope_embedding_cache[embedding_size]
                 freqs_cis_seq_len, _ = freqs_cis.shape
                 if seq_len > freqs_cis_seq_len:
-                    freqs_cis = self.precompute_freqs_cis(embedding_size, seq_len)
+                    freqs_cis = self.precompute_freqs_cis(
+                        embedding_size, seq_len, self.theta
+                    )
                     internal_rope_embedding_cache[embedding_size] = freqs_cis
                 else:
                     freqs_cis = freqs_cis[:seq_len]
             else:
-                freqs_cis = self.precompute_freqs_cis(embedding_size, seq_len)
+                freqs_cis = self.precompute_freqs_cis(
+                    embedding_size, seq_len, self.theta
+                )
                 internal_rope_embedding_cache[embedding_size] = freqs_cis
 
         freqs_real = jnp.tile(freqs_cis.real, (1, 2))
