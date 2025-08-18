e5e5c78ae5caa7dd23f07f42adc1bb4b7cfcaee7
Michael O'Brien <michaelobrien@g.harvard.edu>
2025-08-18 15:34:49 -0400

    careful checking equality for shardings

--- tests/test_sharding.py
+++ tests/test_sharding.py
@@ -2,8 +2,8 @@ import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import jax.sharding as jshard
 import jax.tree_util as jtu
-from jax.sharding import PartitionSpec
 
 jax.config.update("jax_platform_name", "cpu")
 jax.config.update("jax_num_cpu_devices", 2)
@@ -12,14 +12,13 @@ jax.config.update("jax_num_cpu_devices", 2)
 
 def test_sharding_no_inside_jit():
     is_committed = lambda pytree: jax.tree.reduce(lambda x, y: x and y, jax.tree.map(lambda x: x.committed, pytree))
-    is_sharded = lambda pytree, sharding: jax.tree.reduce(lambda x, y: x and y, jax.tree.map(lambda x: x.sharding == sharding, pytree))
+    is_sharded = lambda pytree, sharding: jax.tree.reduce(lambda x, y: x and y, jax.tree.map(lambda x: eqx.tree_equal(x.sharding, sharding), pytree))
 
     mlp = eqx.nn.MLP(2, 2, 2, 2, key=jr.PRNGKey(0))
 
-    devices = jax.local_devices(backend="cpu")
-    num_devices = len(devices)
+    num_devices = 2
     mesh = jax.make_mesh((num_devices,), ("x",))
-    sharding = jax.sharding.NamedSharding(mesh, PartitionSpec("x"))
+    sharding = jshard.NamedSharding(mesh, jshard.PartitionSpec("x"))
     sharded_mlp = eqx.filter_shard(mlp, sharding)
     assert is_committed(eqx.filter(sharded_mlp, eqx.is_array))
     assert is_sharded(eqx.filter(sharded_mlp, eqx.is_array), sharding)
@@ -29,7 +28,7 @@ def test_sharding_no_inside_jit():
         a, b = eqx.partition(x, eqx.is_array)
         a = jtu.tree_map(lambda x: x + 1, a)
         x = eqx.combine(a, b)
-        return a
+        return eqx.filter_shard(a, sharding)
 
     out = f(sharded_mlp)
 
@@ -42,10 +41,11 @@ def test_sharding_only_inside_jit():
     devices = jax.local_devices(backend="cpu")
     num_devices = len(devices)
     mesh = jax.make_mesh((num_devices,), ("x",))
-    sharding = jax.sharding.NamedSharding(mesh, PartitionSpec("x"))
+    sharding = jshard.NamedSharding(mesh, jshard.PartitionSpec("x"))
     # Make dummy pytree
     shape = (num_devices,)
     pytree = ((jnp.zeros(shape), 4), (jnp.zeros(shape),), "something_static")
+    assert not is_sharded(eqx.filter(pytree, eqx.is_array), sharding)
 
     @eqx.filter_jit
     def f(x):
